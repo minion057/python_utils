0f79eed26ecf06471aa7f7ef80869e25dfff38f7
minion057 <getit3981@gmail.com>
2025-09-15 11:13:15 +0900

    Chore: Fix missing quotation marks

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5,
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
